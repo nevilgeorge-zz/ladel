b14d49c1da3af52b13ba6b5091c0988d92b8339e
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-10-04 16:15:04 -0500

    created files for each view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/vote.html',
         controller: 'MainCtrl'
       })
       .when('/about', {
