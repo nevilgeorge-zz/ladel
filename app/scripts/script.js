415ab3e1671d8ff232c39504c3c49387781e08a4
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-10-04 20:54:54 -0500

    styled navbar a tiny bit more

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -4,19 +4,22 @@ $(document).ready(function() {
 
 
 
-
-
-
 	/*
 	 * Events
 	 */
 	// menu animation
-	$('header nav > i').click(function() {
+	$('header nav i').click(function() {
 		if( !$('header nav ul').is(':visible') ) {
-			$('header nav > ul').slideDown();
+			$('header nav ul').slideDown();
 		}
 		else {
-			$('header nav > ul').slideUp();
+			$('header nav ul').slideUp();
 		}
+	});
+	// close menu navbar
+	$('header nav ul li a').click(function() {
+		setTimeout(function() {
+			$('header nav ul').slideUp();
+		}, 1000);
 	});
 });
