65ebb0eeb565561d667a94c99557a3015f16d69b
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-10-05 13:19:33 -0500

    posting new items is possible, but image upload is still not supported

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -11,6 +11,24 @@
 ladelAppControllers.controller('PostCtrl', function ($scope) {
     console.log('Post new');
     
+    $scope.save = function() {
+    	var temp = {
+    		'name': $scope.title,
+    		'image': 'http://highlandercenter.org/wp-content/themes/thalliumity/images/thumbnail.png',
+    		'user': $scope.user,
+    		'votes': 1,
+    		'tagline': $scope.tagline
+    	};
+
+    	dishes.push(temp);
+    	reset();
+    }
+
+    var reset = function() {
+    	$scope.title = "";
+    	$scope.user = "";
+    	$scope.tagline = "";
+    };
 
     
   });
